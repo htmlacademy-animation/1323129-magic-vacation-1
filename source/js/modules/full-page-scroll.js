197f327cea2df9c559c4ea492d180ed630969ffa
mmchalik <mmchalik@domclick.ru>
2021-05-31 16:55:41 +0300

    Анимация перехода на экран призов срабатвыает только после посещения экрана истории и только один раз. Анимации шапки исправлены

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -11,7 +11,9 @@ export default class FullPageScroll {
     this.screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
     this.menuElements = document.querySelectorAll(`.page-header__menu .js-menu-link`);
 
+    this.previousScreen = -1;
     this.activeScreen = 0;
+    this.animationScreenExecuted = false;
     this.onScrollHandler = this.onScroll.bind(this);
     this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
   }
@@ -27,11 +29,13 @@ export default class FullPageScroll {
     const currentPosition = this.activeScreen;
     this.reCalculateActiveScreenPosition(evt.deltaY);
     if (currentPosition !== this.activeScreen) {
+      this.previousScreen = currentPosition;
       this.changePageDisplay();
     }
   }
 
   onUrlHashChanged() {
+    this.previousScreen = this.activeScreen;
     const newIndex = Array.from(this.screenElements).findIndex((screen) => location.hash.slice(1) === screen.id);
     this.activeScreen = (newIndex < 0) ? 0 : newIndex;
     this.changePageDisplay();
@@ -44,31 +48,44 @@ export default class FullPageScroll {
   }
 
   async changeVisibilityDisplay() {
-    const prizesAnimationScreen = document.getElementById(`prizes-animation-screen`);
-    const currentScreenName = this.screenElements[this.activeScreen].id;
-
-    prizesAnimationScreen.classList.remove(`active`);
-
-    if (currentScreenName === `prizes`) {
-      prizesAnimationScreen.classList.add(`active`);
-      await sleep(250);
-    }
+    await this.handlePrizesAnimation();
 
     this.screenElements.forEach((screen) => {
       screen.classList.add(`screen--hidden`);
       screen.classList.remove(`active`);
     });
     this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
-    setTimeout(() => {
+    requestAnimationFrame(() => {
       this.screenElements[this.activeScreen].classList.add(`active`);
-    }, 100);
+    });
+  }
+
+  async handlePrizesAnimation() {
+    const prizesAnimationScreen = document.getElementById(`prizes-animation-screen`);
+    const previousScreen = this.screenElements[this.previousScreen];
+    const previousScreenName = previousScreen && previousScreen.id;
+    const currentScreenName = this.screenElements[this.activeScreen].id;
+
+    prizesAnimationScreen.classList.remove(`active`);
+
+    if (
+      !this.animationScreenExecuted
+      && previousScreenName === `story`
+      && currentScreenName === `prizes`
+    ) {
+      prizesAnimationScreen.classList.add(`active`);
+      await sleep(600);
+      this.animationScreenExecuted = true;
+    }
   }
 
   changeActiveMenuItem() {
     const activeItem = Array.from(this.menuElements).find((item) => item.dataset.href === this.screenElements[this.activeScreen].id);
     if (activeItem) {
       this.menuElements.forEach((item) => item.classList.remove(`active`));
-      activeItem.classList.add(`active`);
+      requestAnimationFrame(() => {
+        activeItem.classList.add(`active`);
+      });
     }
   }
 
